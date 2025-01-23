61523d1a611b5c877b6684141b3e80e438738104
Artem <ionov96artem@mail.com>
2025-01-23 15:33:06 +0400

    All files, add urls and model Task

--- App/models.py
+++ App/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title=models.CharField('Задача', max_length=200)
+    date=models.DateField('Дата окончания')
+    completed=models.BooleanField('Завершено', default=False)
+
+    def __str__(self):
+        return self.title
